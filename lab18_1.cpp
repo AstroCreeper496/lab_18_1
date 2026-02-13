27e9dadddf980d85a87f2538b8d57e71878f84b1
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-02-13 23:04:24 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect a, Rect b){
+    double interw,interh;
+    if (b.x-a.x >= a.w) return 0;
+    else if (b.y-a.y >= b.h) return 0;
+    else {
+        interw = a.x + a.w - b.x;
+        interh = a.y + a.h - b.y;
+    if (a.x <= b.x && b.x + b.w <= a.x + a.w) interw = b.w;
+    if (b.x <= a.x && a.x + a.w <= b.x + b.w) interw = a.w;
+    if (a.y >= b.y && b.y - b.h >= a.y - a.h) interh = b.h;
+    if (b.y >= a.y && a.y - a.h >= b.y - b.h) interh = a.h;
+    //cout << (a.x <= b.x) << (b.x + b.w <= a.x + a.w) <<interw << " " << interh << " ";
+    return interw*interh;
+    }
 }
